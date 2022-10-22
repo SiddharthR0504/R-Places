c12823d5b57cd7e7ae32c7af8ecb4f3027a558c6
SiddharthR0504 <116463680+SiddharthR0504@users.noreply.github.com>
2022-10-22 17:06:52 -0400

    Add files via upload

---
+++ create_db.py
@@ -0,0 +1,14 @@
+#File To Actually Create The Database 
+#Run This File To Create The Database In The Heroku Console
+from app import db, User, Pixel
+
+db.create_all()
+try:
+  Pixel.__table__.create(db.session.bind)
+  User.__table__.create(db.session.bind)
+  "Hello\
+Mane"
+except Exception:
+  print("Already Exist")
+  
+
